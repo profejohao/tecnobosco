54edbf18d646e09f704d99a15f564216f552b70c
profejohao <103144780+profejohao@users.noreply.github.com>
2023-11-02 20:47:24 -0500

    Add files via upload

---
+++ Proyectos/URE/logica/programa.js
@@ -0,0 +1,229 @@
+function color(){
+    var color = document.getElementById("camb");
+    color.style.color = "chocolate";
+    document.getElementById("camb");
+}
+function ctrz(){
+    document.getElementById("camb").style.color = "black";
+}
+function color1(){
+    var color = document.getElementById("camb1");
+    color.style.color = "chocolate";
+    document.getElementById("camb1");
+}
+function ctrz1(){
+    document.getElementById("camb1").style.color = "black";
+}
+function color2(){
+    var color = document.getElementById("camb2");
+    color.style.color = "chocolate";
+    document.getElementById("camb2");
+}
+function ctrz2(){
+    document.getElementById("camb2").style.color = "black";
+}
+function color3(){
+    var color = document.getElementById("camb3");
+    color.style.color = "chocolate";
+    document.getElementById("camb3");
+}
+function ctrz3(){
+    document.getElementById("camb3").style.color = "black";
+}
+function color4(){
+    var color = document.getElementById("camb4");
+    color.style.color = "chocolate";
+    document.getElementById("camb4");
+}
+function ctrz4(){
+    document.getElementById("camb4").style.color = "black";
+}
+function color5(){
+    var color = document.getElementById("camb5");
+    color.style.color = "chocolate";
+    document.getElementById("camb5");
+}
+function ctrz5(){
+    document.getElementById("camb5").style.color = "black";
+}
+function color6(){
+    var color = document.getElementById("camb6");
+    color.style.color = "chocolate";
+    document.getElementById("camb6");
+}
+function ctrz6(){
+    document.getElementById("camb6").style.color = "black";
+}
+function color7(){
+    var color = document.getElementById("camb7");
+    color.style.color = "chocolate";
+    document.getElementById("camb7");
+}
+function ctrz7(){
+    document.getElementById("camb7").style.color = "black";
+}
+function color8(){
+    var color = document.getElementById("camb8");
+    color.style.color = "chocolate";
+    document.getElementById("camb8");
+}
+function ctrz8(){
+    document.getElementById("camb8").style.color = "black";
+}
+function color9(){
+    var color = document.getElementById("camb9");
+    color.style.color = "chocolate";
+    document.getElementById("camb9");
+}
+function ctrz9(){
+    document.getElementById("camb9").style.color = "black";
+}
+
+function boton(){
+    var color = document.getElementById("apoyo");
+    color.style.backgroundColor = "chocolate";
+    color.style.color = "white";
+    document.getElementById("apoyo");
+}
+function ctrzbot(){
+    document.getElementById("apoyo").style.color = "chocolate";
+    document.getElementById("apoyo").style.backgroundColor = "white"
+}
+function botonley(){
+    var color = document.getElementById("boton");
+    color.style.backgroundColor = "white";
+    color.style.color = "darkgreen";
+    document.getElementById("boton");
+}
+function ctrzley(){
+    document.getElementById("boton").style.color = "white";
+    document.getElementById("boton").style.backgroundColor = "rgba(6, 255, 139, 0.445)"
+}
+function vermas(){
+    var color = document.getElementById("Vermas");
+    color.style.backgroundColor = "chocolate";
+    color.style.color = "white";
+    document.getElementById("Vermas");
+}
+function ctrzmas(){
+    document.getElementById("Vermas").style.color = "chocolate";
+    document.getElementById("Vermas").style.backgroundColor = "white"
+}
+function botonvermás(){
+    var color = document.getElementById("botonver");
+    color.style.backgroundColor = "chocolate";
+    color.style.color = "white";
+    document.getElementById("botonver");
+}
+function ctrzvermas(){
+    document.getElementById("botonver").style.color = "chocolate";
+    document.getElementById("botonver").style.backgroundColor = "white"
+}
+function descubremas(){
+    var color = document.getElementById("desmas");
+    color.style.backgroundColor = "chocolate";
+    color.style.color = "white";
+    document.getElementById("desmas");
+}
+function ctrzdesmas(){
+    document.getElementById("desmas").style.color = "chocolate";
+    document.getElementById("desmas").style.backgroundColor = "white"
+}
+function botonvermás2(){
+    var color = document.getElementById("botonver2");
+    color.style.backgroundColor = "chocolate";
+    color.style.color = "white";
+    document.getElementById("botonver2");
+}
+function ctrzvermas2(){
+    document.getElementById("botonver2").style.color = "chocolate";
+    document.getElementById("botonver2").style.backgroundColor = "white"
+}
+function linka1(){
+    var color = document.getElementById("linka1");
+    color.style.color = "black";
+    document.getElementById("linka1");
+}
+function ctrzlink1(){
+    document.getElementById("linka1").style.color = "chocolate";
+}
+function linka2(){
+    var color = document.getElementById("linka2");
+    color.style.color = "black";
+    document.getElementById("linka2");
+}
+function ctrzlink2(){
+    document.getElementById("linka2").style.color = "chocolate";
+}
+function linka3(){
+    var color = document.getElementById("linka3");
+    color.style.color = "black";
+    document.getElementById("linka3");
+}
+function ctrzlink3(){
+    document.getElementById("linka3").style.color = "chocolate";
+}
+function color10(){
+    var color = document.getElementById("camb10");
+    color.style.color = "chocolate";
+    document.getElementById("camb10");
+}
+function ctrz10(){
+    document.getElementById("camb10").style.color = "black";
+}
+function color11(){
+    var color = document.getElementById("camb11");
+    color.style.color = "chocolate";
+    document.getElementById("camb11");
+}
+function ctrz11(){
+    document.getElementById("camb11").style.color = "black";
+}
+function color12(){
+    var color = document.getElementById("camb12");
+    color.style.color = "chocolate";
+    document.getElementById("camb12");
+}
+function ctrz12(){
+    document.getElementById("camb12").style.color = "black";
+}
+function color13(){
+    var color = document.getElementById("camb13");
+    color.style.color = "chocolate";
+    document.getElementById("camb13");
+}
+function ctrz13(){
+    document.getElementById("camb13").style.color = "black";
+}
+function color14(){
+    var color = document.getElementById("camb14");
+    color.style.color = "chocolate";
+    document.getElementById("camb14");
+}
+function ctrz14(){
+    document.getElementById("camb14").style.color = "black";
+}
+function color15(){
+    var color = document.getElementById("camb15");
+    color.style.color = "chocolate";
+    document.getElementById("camb15");
+}
+function ctrz15(){
+    document.getElementById("camb15").style.color = "black";
+}
+function color16(){
+    var color = document.getElementById("camb16");
+    color.style.color = "chocolate";
+    document.getElementById("camb16");
+}
+function ctrz16(){
+    document.getElementById("camb16").style.color = "black";
+}
+function color17(){
+    var color = document.getElementById("camb17");
+    color.style.color = "chocolate";
+    document.getElementById("camb17");
+}
+function ctrz17(){
+    document.getElementById("camb17").style.color = "black";
+}
